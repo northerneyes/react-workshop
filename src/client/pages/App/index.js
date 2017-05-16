819c5e760143a1d893af92227f5fb166028a2ea8
George Buhanov <gbuhanov@gmail.com>
2017-05-16 12:26:44 +0300

    different way to format your messages

--- src/client/pages/App/index.js
+++ src/client/pages/App/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import { FormattedMessage, defineMessages } from 'react-intl';
-import { Grid, PageHeader } from 'react-bootstrap';
+import { object } from 'prop-types';
+
+import {
+  FormattedMessage,
+  defineMessages,
+  FormattedDate,
+  FormattedNumber
+} from 'react-intl';
+import { Grid, PageHeader, FormControl, Button } from 'react-bootstrap';
 
 import Header from '../../components/Header';
 
@@ -8,11 +15,29 @@ export const msg = defineMessages({
   title: {
     id: 'title',
     defaultMessage: 'Building something interesting'
+  },
+  welcome: {
+    id: 'welcome',
+    defaultMessage: `Hello {name}, the date is {date}, you have {unreadCount, number} {unreadCount, plural,
+      one {message}
+      other {messages}
+    } and a lot of money {money}`
+  },
+  inputText: {
+    id: 'inputText',
+    defaultMessage: 'Enter your text here'
+  },
+  clickMe: {
+    id: 'clickMe',
+    defaultMessage: 'Click me'
   }
 });
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
+    const { intl } = this.context;
+    const clickMeMessage = intl.formatMessage(msg.clickMe);
+
     return (
       <div>
         <Header />
@@ -22,9 +47,48 @@ export default class App extends React.Component {
           </Grid>
         </PageHeader>
         <Grid>
-          <h1>Some content</h1>
+          <h3>
+            <FormattedMessage
+              {...msg.welcome}
+              values={{
+                date: (
+                  <FormattedDate
+                    value={Date.now()}
+                    day="numeric"
+                    month="long"
+                    year="numeric"
+                  />
+                ),
+                name: <b>George</b>,
+                money: (
+                  <FormattedNumber
+                    value={1000}
+                    style="currency"
+                    currency="EUR"
+                  />
+                ),
+                unreadCount: 25
+              }}
+            />
+          </h3>
+          <FormattedMessage {...msg.inputText}>
+            {text => (
+              <FormControl
+                id="formControlsText"
+                type="text"
+                placeholder={text}
+              />
+            )}
+          </FormattedMessage>
+          <Button bsStyle="primary">{clickMeMessage}</Button>
         </Grid>
       </div>
     );
   }
 }
+
+App.contextTypes = {
+  intl: object
+};
+
+export default App;
